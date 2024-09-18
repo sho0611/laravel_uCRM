eafc1ce25239e3f997803d66587d7d92645e95e8
Showaa-06 <shogoshogo0611@icloud.com>
2024-09-18 22:38:12 +0900

    secondmessage

---
+++ resources/js/Pages/InertialTest.vue
@@ -0,0 +1,14 @@
+
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+  <Head title="Inertia Test" />
+  Inertiaテストです。 <br>
+  <!-- HTMLのaタグ -->
+  <a href="/">aタグ経由です</a> <br>
+  <!-- Inertia.jsのLinkコンポーネント -->
+  <Link href="/">Linkコンポーネント経由です</Link>
+</template>
